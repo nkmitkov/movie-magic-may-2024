2ea17e49207fd17d7f58bbb3c296293a24b50d1f
Nick <nkmitkov@gmail.com>
2024-05-29 10:08:27 +0300

    Correction 404 page

--- src/constrollers/homeController.js
+++ src/constrollers/homeController.js
@@ -8,7 +8,7 @@ router.get("/about", (req, res) => {
     res.render("about");
 });
 
-router.get("*", (req, res) => {
+router.get("/404", (req, res) => {
     res.render("404");
 });
 
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,11 @@ const router = require("express").Router();
 const homeController = require("./constrollers/homeController");
 const movieController = require("./constrollers/movieController");
 
-router.use(movieController);
 router.use(homeController);
+router.use(movieController);
+
+router.get("*", (req, res) => {
+    res.render("404"); 
+});
 
 module.exports = router;
