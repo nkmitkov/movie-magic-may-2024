5645e33e85bd1fee4c7d8b23b8bdc2918c749366
Nick <nkmitkov@gmail.com>
2024-05-29 10:01:56 +0300

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const homeController = require("./constrollers/homeController");
 const movieController = require("./constrollers/movieController");
 
-router.use(homeController);
 router.use(movieController);
+router.use(homeController);
 
 module.exports = router;
