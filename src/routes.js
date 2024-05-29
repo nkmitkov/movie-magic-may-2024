90ab4c8787a3a1c2df8c0303cd7840b9e3607f5e
Nick <nkmitkov@gmail.com>
2024-05-29 09:34:07 +0300

    Extract routes
    Add home controller

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.render("home", { layout: false });
-});
+const homeController = require("./constrollers/homeController");
+
+router.use(homeController);
 
 module.exports = router;
