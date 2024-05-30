37122e124d8385f9af46ff8f802efccc4bd2955f
Nick <nkmitkov@gmail.com>
2024-05-30 11:23:46 +0300

    Add a new movie to DB

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -7,16 +7,26 @@ const movies = [
         year: '2021',
         imageUrl: '/img/jungle-cruise.jpeg',
         rating: '4',
-        description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.'      
-      }
+        description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.'
+    },
+    {
+        _id: '2',
+        title: 'Home Alone',
+        genre: 'Comedy',
+        director: 'Chris Columbus',
+        year: '1992',
+        imageUrl: '/img/home-alone.jpeg',
+        rating: '4',
+        description: 'Eight-year-old Kevin is accidentally left behind when his family leaves for France. At first, he\'s happy to be in charge; but when thieves try to break into his home, he puts up a fight like no other.'
+    }
 ];
 
-exports.getAll = () => { 
+exports.getAll = () => {
     // return [...movies];
     return movies.slice();
 };
 
-exports.getById = (id) => { 
+exports.getById = (id) => {
     const movie = movies.find((movie) => movie._id == id);
 
     return movie;
