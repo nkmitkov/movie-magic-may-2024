bed9f43d4ee6bd05d5f4a586ba67f10f6afa4aa5
Nick <nkmitkov@gmail.com>
2024-06-03 18:03:11 +0300

    Add mongoose

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const routes = require("./routes");
 const configHandlebars = require("./config/configHandlebars");
@@ -14,4 +15,9 @@ configExpress(app);
 
 app.use(routes);
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+mongoose.connect(`mongodb://localhost:27017/magic-movies`)
+    .then(() => {
+        console.log("DB connected");
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    })
+    .catch((err) => console.log(`DB connection error -> ${err}`));
