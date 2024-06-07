f4da63fcd31acacbd52365479f81948b5725be85
Nick <nkmitkov@gmail.com>
2024-06-07 09:21:02 +0300

    Add register and login page

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 const homeController = require("./constrollers/homeController");
 const movieController = require("./constrollers/movieController");
 const castController = require("./constrollers/castController");
+const authController = require("./constrollers/authController");
 
 router.use(homeController);
 router.use("/movies", movieController);
 router.use("/cast", castController);
+router.use("/auth", authController);
 
 router.get("*", (req, res) => {
     res.render("home/404"); 
