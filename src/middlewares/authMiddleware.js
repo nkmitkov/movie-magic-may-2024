35af423e48e699f14ace03424694aab32533815b
Nick <nkmitkov@gmail.com>
2024-06-07 18:13:09 +0300

    add property for dynamic links in navBar

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -14,6 +14,7 @@ exports.auth = async (req, res, next) => {
         const decodedToken = await jwt.verify(token, SECRET);
 
         req.user = decodedToken;
+        res.locals.isAuthenticated = true;
 
         next();
     } catch (error) {
