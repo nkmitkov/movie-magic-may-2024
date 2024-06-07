fe616cbf807166d3cdf99999fc7a49f7629f5b8a
Nick <nkmitkov@gmail.com>
2024-06-07 11:41:17 +0300

    Add repeat password validation

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -11,8 +12,22 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+        minLength: [8, "Password must be at least 8 characters"],
     }
 })
+
+userSchema.pre("save", async function() {
+    const hash = await bcrypt.hash(this.password, 12)
+    this.password = hash;
+});
+
+userSchema.virtual("rePassword")
+    .set(function(value) {
+        // Validate - ако паролите се различават хвърли грешка, иначе продължи напред
+        if (value !== this.password) {
+            throw new mongoose.MongooseError("Password missmatch!");
+        }
+    });
 
 const User = mongoose.model("User", userSchema);
 
